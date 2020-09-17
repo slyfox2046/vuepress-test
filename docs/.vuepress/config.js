3dfc9c9c3d8347c9a58e34de7c17a1a076ebcf2a
slyfox2046 <slyfox2046@gmail.com>
2020-09-17 08:48:28 +0800

    vuepress pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,6 +33,15 @@ module.exports = {
     ['meta', { name: 'author', content: 'sly' }],
     ['meta', { name: 'keywords', content: 'vuepress介绍,vuepress说明,飞跃高山' }],
     ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
+    // pwa
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     lastUpdated: '更新时间',
